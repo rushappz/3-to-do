942258297c459638095eb28728486e4abeab1a1b
Rushanthan <rushan26@live.com>
2017-03-23 11:26:33 +0530

    Deleting records in firebase via the app

--- www/js/controller.js
+++ www/js/controller.js
@@ -1,11 +1,25 @@
-app.controller('todoCtrl', function($scope, $firebaseArray, $state, todoService) {
+app.controller('todoCtrl', function($scope, $firebaseArray, $state, todoService, $ionicActionSheet) {
+	$scope.todo = todoService.all;
+	
 	$scope.saveTodo = function(){
 		$scope.newTodo = todoService.all;
 		$scope.newTodo.$add({
 			name: $scope.name
 		});
 		
-		$scope.name = '';
 		$state.go('todo');
+	}
+	
+	$scope.removeTodo = function(id) {
+		$ionicActionSheet.show({
+			destructiveText: 'Remove Todo',
+			titleText: 'Are you sure?',
+			cancelText: 'Cancel',
+			destructiveButtonClicked: function(){
+				var rem = $scope.todo.$getRecord(id);
+				$scope.todo.$remove(rem);
+				return true;
+			}
+		});
 	}
 });
